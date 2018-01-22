574f2b1c63ee7873a7eb462803ce9de14adc327f
Leslie S. Smith <l.s.smith@cs.stir.ac.uk>
2018-01-22 15:36:34 +0000

    minor changes, fixes

--- bandpass_signal.m
+++ bandpass_signal.m
@@ -1,10 +1,10 @@
-function signal_out = bandpass_signal(signal_in,hipass, lowpass)
+function signal_out = bandpass_signal(signal_in,lowpass, hipass)
 %bandpass_signal bandpass signal_in
 %   simple bandpass filer applied to an input signal
 SAMPLERATE = 44100 ;
 filter_order = 10 ;
 % generate bandpass filter
-BPF = fdesign.bandpass('N,F3dB1,F3dB2',filter_order, hipass, lowpass, SAMPLERATE) ;
+BPF = fdesign.bandpass('N,F3dB1,F3dB2',filter_order, lowpass, hipass, SAMPLERATE) ;
 f_BPF = design(BPF) ;
 % perform the filtering
 signal_out = filter(f_BPF, signal_in) ;
