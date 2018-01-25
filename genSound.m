813216ef0d119cf9afee3a4b1b3ae9051800b2d9
Leslie S. Smith <l.s.smith@cs.stir.ac.uk>
2018-01-25 20:18:36 +0000

    altered genSound to use three parameters: duration, pitch, and harmonic content.

--- genSound.m
+++ genSound.m
@@ -1,23 +1,19 @@
-function [signalOut] = genSound(duration,pitch)
-%genSound Simple test of sound generation
-%   Detailed explanation goes here
-sampleRate = 44100 ; % standard sample rate
-risetime = 0.01 ;
-falltime = 0.02 ;
-% signalOut = zeros([1 duration * sampleRate]) ;
+function [signalOut] = genSound(duration,pitch, harmonics)
+%genSound sound generation at pitch with harmonics
+%   Generate a sound phase starting at 0 of durtion at pitch, with harmonic
+%   content defined by harmonics array
+SAMPLERATE = 44100 ; % standard sample rate
+
+signalOut = zeros([1 duration * SAMPLERATE]) ;
 % 
-% simple computation
-signalOut = sin(2 * pi * pitch * ([1 : duration * sampleRate] / sampleRate) ) ;
-% implement rise time: modulate start by  quarter cycle of sin wave with duration
-% risetime
-omegarise = 1/(4 * risetime) ;
-risemodulator = sin(2 * pi * omegarise * [1:risetime * sampleRate]/sampleRate) ; 
-signalOut(1:length(risemodulator)) = signalOut(1:length(risemodulator)) ...
-    .* risemodulator ;
-% implement fall time: modulate end by 2nd pi/2 quarter cycle of sine wave.
-omegafall = 1/(4 * falltime) ;
-fallmodulator = sin(pi/2 + 2 * pi * omegafall * [1:falltime*sampleRate]/sampleRate) ; 
-signalOut(end-length(fallmodulator) + 1: end) = signalOut(end-length(fallmodulator) + 1: end) ...
-    .* fallmodulator ;
+% normalise harmonics so that sum  is 1 
+harmonics = harmonics / sum(sqrt(harmonics .* harmonics)) ;
+
+% signal computation
+for i = 1:length(harmonics)
+    signalOut = signalOut + harmonics(i) * ... 
+        sin(2* i * pi * pitch * ([1 : duration * SAMPLERATE] / SAMPLERATE) ) ;
+end
+
 end
 
